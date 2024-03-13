027d7063356f5a93bd015bf6fbe050ff82e2ff7a
ivannet123 <ivannet123@github.com>
2024-03-13 11:36:43 +0300

    Добавлен сценарий добавления нового контакта и соответствующий вспомогательный класс

---
+++ contact.py
@@ -0,0 +1,26 @@
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname , title, company, address,
+                 home_phone, mobile_phone, work_phone, fax_phone, email1, email2, email3, homepage,
+                 bday_day, bday_month, bday_year, ann_day, ann_month, ann_year):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.fax_phone = fax_phone
+        self.email1 = email1
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.bday_day = bday_day
+        self.bday_month = bday_month
+        self.bday_year = bday_year
+        self.ann_day = ann_day
+        self.ann_month = ann_month
+        self.ann_year = ann_year
